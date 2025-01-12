f93b8d1bd06b72a6a1ad930f098cbd1ceb3e88b5
OmerKilic <omeerfkiliic@gmail.com>
2025-01-12 11:20:57 +0300

    Created Scriptable Objcet Observer Pattern items

---
+++ Assets/Scripts/SOs/Editor/EventEditor.cs
@@ -0,0 +1,21 @@
+using SOs.Events;
+using UnityEditor;
+using UnityEngine;
+
+namespace SOs.Editor
+{
+    [CustomEditor(typeof(GameEvent), editorForChildClasses: true)]
+    public class EventEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            GameEvent e = target as GameEvent;
+            if (GUILayout.Button("Raise"))
+                e.Raise();
+        }
+    }
+}
---
+++ Assets/Scripts/SOs/Events/GameEventListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SOs.Events
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [Tooltip("Event to register with.")] public GameEvent Event;
+
+        [Tooltip("Response to invoke when event is raised.")]
+        public UnityEvent Response;
+
+        private void OnEnable()
+        {
+            Event.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            Event.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            Response.Invoke();
+        }
+    }
+}
