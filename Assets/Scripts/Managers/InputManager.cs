a0bad61f9c76dbfbc152f13ad3dc0ee16cfa9374
OmerKilic <omeerfkiliic@gmail.com>
2025-01-13 01:59:14 +0300

    Created Input Manager

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    public UnityEvent<Vector2> OnTouchBegan;
+    public UnityEvent<Vector2> OnTouchMoved;
+    public UnityEvent<Vector2> OnTouchCancelled;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    OnTouchBegan.Invoke(touch.position);
+                    break;
+
+                case TouchPhase.Moved:
+                    OnTouchMoved.Invoke(touch.deltaPosition);
+                    break;
+
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    OnTouchCancelled.Invoke(touch.position);
+                    break;
+            }
+        }
+    }
+}
