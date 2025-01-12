e815ee26968f4029d24959241d16dd287d5ad114
OmerKilic <omeerfkiliic@gmail.com>
2025-01-13 01:58:32 +0300

    Created PlayerHierarchController
    -Handles players position with and without stick

---
+++ Assets/Scripts/Player/PlayerHierarchyController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerHierarchyController : MonoBehaviour
+{
+    [SerializeField] private GameObject _tipOfStick;
+    [SerializeField] private Vector3 _playerStartPosition = new Vector3(0, 26.35f, 0);
+
+    public void MakeItSticksChild()
+    {
+        if (_tipOfStick == null)
+        {
+            Debug.LogError("No tip of stick");
+            return;
+        }
+
+        transform.SetParent(_tipOfStick.transform);
+        transform.position = _tipOfStick.transform.position;
+    }
+
+    public void MakeItFree()
+    {
+        transform.SetParent(null);
+        transform.position = _playerStartPosition;
+    }
+}
