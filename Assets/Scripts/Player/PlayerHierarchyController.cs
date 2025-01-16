c1401666658f0eeec9a6f9d0db0105bb9c13750a
OmerKilic <omeerfkiliic@gmail.com>
2025-01-16 04:26:35 +0300

    Update PlayerHierarchyController.cs
    -coding convention changes

--- Assets/Scripts/Player/PlayerHierarchyController.cs
+++ Assets/Scripts/Player/PlayerHierarchyController.cs
@@ -1,25 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerHierarchyController : MonoBehaviour
 {
-    [SerializeField] private GameObject _tipOfStick;
-    [SerializeField] private Vector3 _playerStartPosition = new Vector3(0, 26.35f, 0);
+    [SerializeField] private GameObject tipOfStick;
+    [SerializeField] private Vector3 playerStartPosition = new Vector3(0, 26.35f, 0);
 
     public void MakeItSticksChild()
     {
-        if (_tipOfStick == null)
+        if (tipOfStick == null)
         {
             Debug.LogError("No tip of stick");
             return;
         }
 
-        transform.SetParent(_tipOfStick.transform);
-        transform.position = _tipOfStick.transform.position;
+        transform.SetParent(tipOfStick.transform);
+        transform.position = tipOfStick.transform.position;
     }
 
     public void MakeItFree()
     {
         transform.SetParent(null);
-        transform.position = _playerStartPosition;
+        transform.position = playerStartPosition;
     }
 }
