facbe4ef013649a10c0d5ce128bc3710c9cc34e9
OmerKilic <omeerfkiliic@gmail.com>
2025-01-16 04:30:21 +0300

    More robust Input manager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,11 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class InputManager : MonoBehaviour
 {
-    public UnityEvent<Vector2> OnTouchBegan;
-    public UnityEvent<Vector2> OnTouchMoved;
-    public UnityEvent<Vector2> OnTouchCancelled;
+    [Header("Player Input Events")]
+    [SerializeField] private UnityEvent<Vector2> onTouchBegan;
+    [SerializeField] private UnityEvent<Vector2> onTouchMoved;
+    [SerializeField] private UnityEvent<Vector2> onTouchCancelled;
+    
+    [Header("Stick Input Events")]
+    [SerializeField] private UnityEvent<Vector2> onStickBegan;
+    [SerializeField] private UnityEvent<Vector2> onStickMoved;
+    [SerializeField] private UnityEvent<Vector2> onStickEnded;
+
+    [Header("Event To Start The Game")]
+    [SerializeField] private VoidGameEvent onStartGame;
+
+    private bool _isStickState = true;
+
+    private void Start()
+    {
+        onStartGame.Raise();
+        _isStickState = true;
+    }
 
     private void Update()
     {
@@ -13,21 +31,44 @@ public class InputManager : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
 
-            switch (touch.phase)
+            if (_isStickState)
             {
-                case TouchPhase.Began:
-                    OnTouchBegan.Invoke(touch.position);
-                    break;
-
-                case TouchPhase.Moved:
-                    OnTouchMoved.Invoke(touch.position);
-                    break;
-
-                case TouchPhase.Ended:
-                case TouchPhase.Canceled:
-                    OnTouchCancelled.Invoke(touch.position);
-                    break;
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        onStickBegan?.Invoke(touch.position);
+                        break;
+                    case TouchPhase.Moved:
+                        onStickMoved?.Invoke(touch.position);
+                        break;
+                    case TouchPhase.Ended:
+                    case TouchPhase.Canceled:
+                        onStickEnded?.Invoke(touch.position);
+                        break;
+                }
+            }
+            
+            else
+            {
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        onTouchBegan?.Invoke(touch.position);
+                        break;
+                    case TouchPhase.Moved:
+                        onTouchMoved?.Invoke(touch.position);
+                        break;
+                    case TouchPhase.Ended:
+                    case TouchPhase.Canceled:
+                        onTouchCancelled?.Invoke(touch.position);
+                        break;
+                }
             }
         }
+    }
+    
+    public void SetInputStateToStick(bool state)
+    {
+        _isStickState = state;
     }
 }
